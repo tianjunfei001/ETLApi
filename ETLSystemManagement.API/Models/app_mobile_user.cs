ea1039d3e6daadeaf9aabaa3900b50737d90293f
tianjunfei001 <tianjunfei001@163.com>
2021-05-24 18:59:30 +0800

    003

---
+++ ETLSystemManagement.API/Models/app_mobile_user.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ETLSystemManagement.API.Models
+{
+    public class app_mobile_user
+    {
+
+        public long id { get; set; }
+        /// <summary>
+        /// 手机号
+        /// </summary>
+        public string phone { get; set; }
+        /// <summary>
+        /// 密码
+        /// </summary>
+        public string password { get; set; }
+
+
+    }
+}
