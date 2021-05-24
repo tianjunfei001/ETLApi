ffbbe618c27d5c2de7c2bdde3252406c4434c4fb
tianjunfei001 <tianjunfei001@163.com>
2021-05-24 20:07:13 +0800

    测试 token令牌

--- ETLSystemManagement.API/Controllers/CompanyController.cs
+++ ETLSystemManagement.API/Controllers/CompanyController.cs
@@ -3,9 +3,16 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using ETL.ERP.BLL;
 using ETL.ERP.Model;
+using ETLSystemManagement.API.Models;
+using Microsoft.Extensions.Options;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Authorization;
+using IdentityModel;
 
 namespace ETLSystemManagement.API.Controllers
 {
@@ -13,13 +20,88 @@ namespace ETLSystemManagement.API.Controllers
     [ApiController]
     public class CompanyController : ControllerBase
     {
+
+        //获取JwtSettings对象信息
+        private JwtSettings _jwtSettings;
+
         //注入
         Company_BLL company_BLL;// = new Company_BLL();
 
-        public CompanyController(Company_BLL _company_BLL)
+        public CompanyController(Company_BLL _company_BLL, IOptions<JwtSettings> _jwtSettingsAccesser)
         {
             company_BLL = _company_BLL;
+            _jwtSettings = _jwtSettingsAccesser.Value;
         }
+
+
+        /// <summary>
+        /// 获取token
+        /// </summary>
+        private object Token(app_mobile_user model)
+        {
+            //测试自己创建的对象
+            var user = new app_mobile_user
+            {
+                id = 1,
+                phone = "138000000",
+                password = "e10adc3949ba59abbe56e057f20f883e"
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var key = Encoding.UTF8.GetBytes(_jwtSettings.SecretKey);
+            var authTime = DateTime.Now;//授权时间
+            var expiresAt = authTime.AddDays(30);//过期时间
+            var tokenDescripor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[] {
+                    new Claim(JwtClaimTypes.Audience,_jwtSettings.Audience),
+                    new Claim(JwtClaimTypes.Issuer,_jwtSettings.Issuer),
+                    new Claim(JwtClaimTypes.Name, user.phone.ToString()),
+                    new Claim(JwtClaimTypes.Id, user.id.ToString()),
+                    new Claim(JwtClaimTypes.PhoneNumber, user.phone.ToString())
+                }),
+                Expires = expiresAt,
+                //对称秘钥SymmetricSecurityKey
+                //签名证书(秘钥，加密算法)SecurityAlgorithms
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescripor);
+            var tokenString = tokenHandler.WriteToken(token);
+            var result = new
+            {
+                access_token = tokenString,
+                token_type = "Bearer",
+                profile = new
+                {
+                    id = user.id,
+                    name = user.phone,
+                    phone = user.phone,
+                    auth_time = authTime,
+                    expires_at = expiresAt
+                }
+            };
+            return result;
+        }
+
+        [Route("get_token")]
+        [HttpPost]
+        public IActionResult GetToken()
+        {
+            return Ok(Token(null));
+        }
+
+        [Authorize]
+        [Route("get_user_info")]
+        [HttpPost]
+        public IActionResult GetUserInfo()
+        {
+            //获取当前请求用户的信息，包含token信息
+            var user = HttpContext.User;
+            return Ok("15563");
+        }
+
+
+
 
         /// <summary>
         /// 获取company公司基本信息表
@@ -52,7 +134,7 @@ namespace ETLSystemManagement.API.Controllers
             return Ok(new
             {
                 code = 0,
-                msg = h>=1?true:false,
+                msg = h >= 1 ? true : false,
 
             });
         }
@@ -65,13 +147,13 @@ namespace ETLSystemManagement.API.Controllers
         [HttpGet, Route("GetFanCompan")]
         public IActionResult GetFanCompan(int id)
         {
-            Company list = company_BLL.GetFanTable<Company>(id,"id");
+            Company list = company_BLL.GetFanTable<Company>(id, "id");
 
             return Ok(new
             {
                 code = 0,
                 msg = "",
-                data=list
+                data = list
             });
         }
 
@@ -80,7 +162,7 @@ namespace ETLSystemManagement.API.Controllers
         /// </summary>
         /// <param name="model"></param>
         /// <returns></returns>
-        [HttpPost,Route("PostUpdCompan")]
+        [HttpPost, Route("PostUpdCompan")]
         public IActionResult PostUpdCompan(Company model)
         {
             int h = company_BLL.GetUpdateTable<Company>(model, "id");
